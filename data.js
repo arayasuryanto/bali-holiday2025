7da63dee9212741369b7e4dce92e712962ce39c4
arayasuryanto <121339156+arayasuryanto@users.noreply.github.com>
2025-04-06 21:23:10 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,43 @@
+const expenseData = [
+    // D0
+    { Hari: "D0", Deskripsi: "Hotel Ananta legian - Legian", "Jumlah (Rp)": 2054088, Kategori: "Akomodasi" },
+    { Hari: "D0", Deskripsi: "Hotel Yuda menjangan homestay - Menjangan", "Jumlah (Rp)": 486000, Kategori: "Akomodasi" },
+    
+    // D1
+    { Hari: "D1", Deskripsi: "Tiket kapal Surabaya - Bali", "Jumlah (Rp)": 216000, Kategori: "Transportasi" },
+    { Hari: "D1", Deskripsi: "Sarapan kecil - Rest area tepi pantai", "Jumlah (Rp)": 29000, Kategori: "Makanan & Minuman" },
+    { Hari: "D1", Deskripsi: "Makan siang Taliwang Bersaudara - Kuta", "Jumlah (Rp)": 358000, Kategori: "Makanan & Minuman" },
+    { Hari: "D1", Deskripsi: "Oleh-oleh Krisna - Sunset Road", "Jumlah (Rp)": 655000, Kategori: "Belanja & Oleh-oleh" },
+    { Hari: "D1", Deskripsi: "Makan malam seafood - Pantai Doublesix Legian", "Jumlah (Rp)": 263000, Kategori: "Makanan & Minuman" },
+    { Hari: "D1", Deskripsi: "Belanja Indomaret sebelum tidur - Legian", "Jumlah (Rp)": 154000, Kategori: "Belanja & Konsumsi" },
+    
+    // D2
+    { Hari: "D2", Deskripsi: "Masuk pasar Pandonganan 1 - Pandonganan", "Jumlah (Rp)": 5000, Kategori: "Transportasi" },
+    { Hari: "D2", Deskripsi: "Parkir mall - Mall Bali Galeria", "Jumlah (Rp)": 6000, Kategori: "Transportasi" },
+    { Hari: "D2", Deskripsi: "J.CO mini donuts - Mall Bali Galeria", "Jumlah (Rp)": 61000, Kategori: "Makanan & Minuman" },
+    { Hari: "D2", Deskripsi: "Uniqlo - Mall Bali Galeria", "Jumlah (Rp)": 299000, Kategori: "Lain-lain" },
+    { Hari: "D2", Deskripsi: "Photo booth mall - Mall Bali Galeria", "Jumlah (Rp)": 30000, Kategori: "Lain-lain" },
+    { Hari: "D2", Deskripsi: "Dexlite - Denpasar", "Jumlah (Rp)": 400000, Kategori: "Transportasi" },
+    { Hari: "D2", Deskripsi: "Mie ayam bakso - Bakso Morlando", "Jumlah (Rp)": 187000, Kategori: "Makanan & Minuman" },
+    { Hari: "D2", Deskripsi: "M Mart - Jl. Raya Canggu", "Jumlah (Rp)": 152000, Kategori: "Belanja & Konsumsi" },
+    { Hari: "D2", Deskripsi: "Masuk pasar Pandonganan 2 - Pandonganan", "Jumlah (Rp)": 5000, Kategori: "Transportasi" },
+    { Hari: "D2", Deskripsi: "Ikan-ikan - Pasar Pandonganan", "Jumlah (Rp)": 343000, Kategori: "Makanan & Minuman" },
+    { Hari: "D2", Deskripsi: "Biaya bakar ikan - Pak Hj. Sodiq", "Jumlah (Rp)": 85000, Kategori: "Makanan & Minuman" },
+    { Hari: "D2", Deskripsi: "Parkir - Pandonganan", "Jumlah (Rp)": 10000, Kategori: "Transportasi" },
+    
+    // D3
+    { Hari: "D3", Deskripsi: "Wisma Mart - Jl. Raya Kuta", "Jumlah (Rp)": 159000, Kategori: "Belanja & Konsumsi" },
+    { Hari: "D3", Deskripsi: "Pie Susu Dhian - Bali", "Jumlah (Rp)": 461000, Kategori: "Belanja & Oleh-oleh" },
+    { Hari: "D3", Deskripsi: "Masuk Bedugul - Bedugul", "Jumlah (Rp)": 10000, Kategori: "Transportasi" },
+    { Hari: "D3", Deskripsi: "Makan Ayam Taliwang - Bedugul Hj. Shidiq", "Jumlah (Rp)": 328000, Kategori: "Makanan & Minuman" },
+    { Hari: "D3", Deskripsi: "Nasi goreng & Mie - Area Menjangan", "Jumlah (Rp)": 36000, Kategori: "Makanan & Minuman" },
+    { Hari: "D3", Deskripsi: "Jaya Fried Chicken - Area Menjangan", "Jumlah (Rp)": 76000, Kategori: "Makanan & Minuman" },
+    
+    // D4
+    { Hari: "D4", Deskripsi: "Tiket Kapal Bali - Surabaya", "Jumlah (Rp)": 216000, Kategori: "Transportasi" },
+    { Hari: "D4", Deskripsi: "Nasi Tempong - Banyuwangi", "Jumlah (Rp)": 170000, Kategori: "Makanan & Minuman" },
+    { Hari: "D4", Deskripsi: "Ikan Asap - Probolinggo", "Jumlah (Rp)": 160000, Kategori: "Makanan & Minuman" },
+    { Hari: "D4", Deskripsi: "Oleh-oleh tape - Probolinggo", "Jumlah (Rp)": 135000, Kategori: "Belanja & Oleh-oleh" },
+    { Hari: "D4", Deskripsi: "Dexlite - Tol Probolinggo", "Jumlah (Rp)": 500000, Kategori: "Transportasi" },
+    { Hari: "D4", Deskripsi: "Tol - Tol Gempol", "Jumlah (Rp)": 76000, Kategori: "Transportasi" }
+  ];
